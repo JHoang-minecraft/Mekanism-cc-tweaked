35fce50b37609660ec3ac1cdcb66035b9e2b6a22
HycoreMC <hoangnenguyen907@gmail.com>
2025-08-19 14:51:24 +0700

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,17 +1,22 @@
 
-if not fs.exists("APISystem.APICore.lua") then
+local url = "https://raw.githubusercontent.com/JHoang-minecraft/Mekanism-cc-tweaked/refs/heads/main/APISystem/APICore.lua"
+
+local response = http.get(url)
+if not response then
     error("APPLICATION CRASHER! MISSING APICORE")
 end
 
-local API = require("APISystem.APICore") 
+local code = response.readAll()
+response.close()
+
+local API = load(code)()
 print("APICore loaded successfully!")
 
 local packages = {"GPU.package", "System.package", "menu.package", "API.package"}
-
 for _, pkg in ipairs(packages) do
     for i = 1, 20 do
         print("Loading " .. pkg .. " (" .. i .. "/20)")
     end
 end
 
-print("Application Install complete! Enter Configure =))")
+print("Application Install complete! Enter Configure")
