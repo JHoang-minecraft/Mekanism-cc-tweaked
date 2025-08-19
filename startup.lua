b8d484d903c544f4bb256872abe9181a1ad213a9
HycoreMC <hoangnenguyen907@gmail.com>
2025-08-19 13:04:14 +0700

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -73,6 +73,10 @@ local function drawMenu()
   monitor.write("│ 0. Exit Program  │")
   monitor.setCursorPos(2,17)
   monitor.write("└─────────────────┘")
+  monitor.setTextScale(0.5)
+  monitor.setBackgroundColor(colors.black)
+  monitor.setTextColor(colors.white)
+  monitor.clear()
 end
 
 local function updateStatus()
