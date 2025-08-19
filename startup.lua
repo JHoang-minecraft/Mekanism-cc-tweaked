c31d88c0cc9aa686b1bdfa4dbe0fab7ba158deff
HycoreMC <hoangnenguyen907@gmail.com>
2025-08-19 12:24:28 +0700

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,4 +1,4 @@
-
+-- Tìm monitor và reactor logic adapter
 local monitor, adapter
 
 for _, side in pairs({"top","bottom","left","right","front","back"}) do
@@ -10,9 +10,12 @@ for _, side in pairs({"top","bottom","left","right","front","back"}) do
         adapter = peripheral.wrap(side)
     end
 end
+
+-- Kiểm tra kết nối
 if not monitor then print("Lỗi: Gắn monitor vào computer!") return end
 if not adapter then print("Lỗi: Đặt computer áp Logic Adapter!") return end
 
+-- Hiệu ứng loading
 local function loading()
     monitor.clear()
     monitor.setCursorPos(1,1)
@@ -22,6 +25,7 @@ local function loading()
     end
 end
 
+-- Vẽ menu cảm ứng
 local function drawMenu()
     monitor.clear()
     monitor.setCursorPos(2,2)
@@ -32,24 +36,26 @@ local function drawMenu()
     monitor.write("[0] THOÁT")
 end
 
+-- Chạy chương trình
 loading()
 drawMenu()
 
 while true do
     local event, side, x, y = os.pullEvent("monitor_touch")
     
-    if y == 2 then  
+    -- Xử lý chạm vào vị trí menu
+    if y == 2 then  -- Chạm vào dòng 2 (BẬT/TẮT)
         local status = adapter.getStatus()
         adapter.setActive(status == "inactive")
         monitor.setCursorPos(15,2)
         monitor.write("["..(status == "active" and "TẮT" or "BẬT").."]")
-    elseif y == 4 then  
+    elseif y == 4 then  -- Chạm vào dòng 4 (DỪNG KHẨN)
         adapter.setActive(false)
         monitor.setCursorPos(15,4)
         monitor.write("[ĐÃ DỪNG!]")
         sleep(1)
         drawMenu()
-    elseif y == 6 then 
+    elseif y == 6 then  -- Chạm vào dòng 6 (THOÁT)
         break
     end
 end
